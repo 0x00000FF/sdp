e5ebbc58caad959911a952b561b22014db05c40e
Kangjun Heo <knowledge@o.cnu.ac.kr>
2024-09-12 15:25:59 +0900

    feat: :sparkles: Implement JSON parsing into SPAMessage in handle_spa

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,18 @@ use tokio::net::{TcpListener, TcpStream, UdpSocket};
  *
  */
 
- #[derive(Serialize, Deserialize)]
+ #[derive(Debug, Serialize, Deserialize)]
 pub struct SPAMessage {
-    client_id      : [u8; 32],
+    client_id      : String,
     nonce          : u16,
     timestamp      : u64,
-    source         : Vec<u8>,   // Must be 4 bytes or 16 bytes
+    source         : String,   // Must be 4 bytes or 16 bytes
     
     message_type   : u8,        // SEE MessageType
-    message_string : Vec<u8>,
+    message_string : Option<String>,
 
-    hotp           : Vec<u8>,
-    hmac           : Vec<u8>
+    hotp           : String,
+    hmac           : String
 }
 
 const SPA_PREAMBLE_TEXT_SIZE:usize = 8; // SPDSPA<NUL><NUL>
@@ -32,7 +32,7 @@ const SPA_PREAMBLE_MSG_SIZE:usize  = 8; // 8 bytes per each message
 
 const SPA_PREAMBLE_SIZE:usize      = SPA_PREAMBLE_TEXT_SIZE + SPA_PREAMBLE_MSG_SIZE;
 
-const SPA_PREAMBLE_TEXT:u64        = 0x5350445350410000; // SPDSPA<NUL><NUL>
+// const SPA_PREAMBLE_TEXT:u64        = 0x5350445350410000; // SPDSPA<NUL><NUL>
 const SPA_PREAMBLE_TEXT_LE:u64     = 0x0000415053445053; // LITTLE-ENDIAN
 
 const BUF_SIZE: usize              = 1024;
@@ -48,14 +48,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("debug: starting tcp thread");
 
     loop {
-        let (mut socket, _) = tcp_listener.accept().await?;
+        let (socket, _) = tcp_listener.accept().await?;
         tokio::spawn(accept_socket(socket));
     }
 }
 
 async fn handle_spa(peer: SocketAddr, buff: Vec<u8>) {
-    let json = String::from_utf8(buff);
-    
+    let json_str = match String::from_utf8(buff) {
+        Ok(str) => str,
+        Err(_) => panic!("Invalid UTF-8 String")
+    };
+    let message: SPAMessage = serde_json::from_str(&json_str).unwrap();
+
+    println!("{:?}", message);
 }
 
 async fn wait_for_spa(socket: UdpSocket) {
@@ -84,8 +89,10 @@ async fn wait_for_spa(socket: UdpSocket) {
 
             // GET SPA MESSAGE SIZE AND CHECK WITH TOTAL RECEIVED SIZE
             // ASSERT( TOTAL RECEIVED - PREAMBLE SIZE == SPA MESSAGE SIZE IN PREAMBLE )
-            let msg_size_bytes: [u8; 8] = buff[SPA_PREAMBLE_TEXT_SIZE..SPA_PREAMBLE_MSG_SIZE].try_into().unwrap();
+            let msg_size_bytes: [u8; 8] = buff[SPA_PREAMBLE_TEXT_SIZE..SPA_PREAMBLE_SIZE].try_into().unwrap();
             let msg_size = usize::from_le_bytes(msg_size_bytes);
+
+            println!("message received bytes: {}", msg_size);
 
             if msg_size != (size - SPA_PREAMBLE_SIZE) {
                 println!("debug: total message size not valid (in packet: {}, actual: {})", msg_size, size - SPA_PREAMBLE_MSG_SIZE);
@@ -95,7 +102,8 @@ async fn wait_for_spa(socket: UdpSocket) {
             }
 
             // GET SPA MESSAGE BUFFER
-            let spa_body = buff[SPA_PREAMBLE_SIZE..msg_size].to_vec();
+            let spa_body_end = SPA_PREAMBLE_SIZE + msg_size;
+            let spa_body = buff[SPA_PREAMBLE_SIZE..spa_body_end].to_vec();
 
             // HANDLE SINGLE PACKET AUTHORIZATION
             tokio::spawn(handle_spa(peer, spa_body));
